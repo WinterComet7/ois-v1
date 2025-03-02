87afd0dd2403fea0977c5d41a4da648479b7bb5a
WinterComet7 <zz28033@student.uni-lj.si>
2025-03-02 14:27:26 +0100

    Dodaj izračun pospeška

---
+++ GravitacijaB.java
@@ -0,0 +1,6 @@
+public class GravitacijaB {
+    public static double pospesek(double visina) {
+        double rezultat = ( (6.674 * Math.pow(10, -11) * 5.972 * Math.pow(10, 24)) / (Math.pow(6.371 * Math.pow(10, 6) + visina, 2)));
+        return rezultat;
+    }
+}
